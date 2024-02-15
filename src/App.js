aef7455fb80b08980b61e456f92f5d7eb0f79784
duongth <duongth>
2024-02-16 03:06:01 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 import { useCallback, useState } from "react";
-import { v4 as uuid } from "uuid";
+import { FaPlus, FaTrash } from "react-icons/fa";
+import Modal from "react-modal";
 import ReactFlow, {
-  useNodesState,
-  useEdgesState,
   Position,
   addEdge,
-  NodeToolbar,
-  ControlButton,
-  Controls,
+  useEdgesState,
+  useNodesState,
 } from "reactflow";
 import "reactflow/dist/style.css";
-import Modal from "react-modal";
+import { v4 as uuid } from "uuid";
 import "./index.css";
-
-const initialNodes = [
-  {
-    id: "1",
-    data: { label: "Node gốc" },
-    position: { x: 100, y: 100 },
-    sourcePosition: Position.Right,
-    targetPosition: Position.Left,
-    forceToolbarVisible: true,
-  },
-];
 
 const initialEdges = [];
 
 const TouchDeviceFlow = () => {
-  const [nodes, seNodes, onNodesChange] = useNodesState(initialNodes);
+  const initialNodes = [
+    {
+      id: "1",
+      data: {
+        label: (
+          <div>
+            <div>Node gốc</div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "2px",
+              }}
+            >
+              <div
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  setCurrentNode("1");
+                  setIsOpen(true);
+                }}
+              >
+                <FaPlus />
+              </div>
+              {/* <div  onClick={() => {}}>
+                <FaTrash />
+              </div> */}
+            </div>
+          </div>
+        ),
+      },
+      position: { x: 100, y: 100 },
+      sourcePosition: Position.Right,
+      targetPosition: Position.Left,
+      forceToolbarVisible: true,
+    },
+  ];
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [currentNode, setCurrentNode] = useNodesState(null);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const [isOpen, setIsOpen] = useState(false);
@@ -36,41 +60,49 @@ const TouchDeviceFlow = () => {
     setEdges((eds) => addEdge(connection, eds));
   }, []);
 
-  const addNode = () => {
-    seNodes([
-      ...nodes,
-      {
-        id: "4",
-        data: { label: "Node 3" },
-        position: { x: 300, y: 100 },
-        sourcePosition: Position.Right,
-        targetPosition: Position.Left,
-      },
-    ]);
-
-    setEdges((eds) =>
-      addEdge(
-        {
-          source: "1",
-          sourceHandle: null,
-          target: "2",
-          targetHandle: null,
-        },
-        eds
-      )
-    );
-  };
-
   const onSubmit = () => {
     const id = uuid();
 
-    console.log(id);
-    seNodes([
+    setNodes([
       ...nodes,
       {
         id: id,
-        data: { label: nameNode },
-        position: { x: 300, y: 100 },
+        data: {
+          label: (
+            <div>
+              <div>{nameNode}</div>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  marginTop: "2px",
+                }}
+              >
+                <div
+                  style={{ cursor: "pointer", marginRight: "5px" }}
+                  onClick={() => {
+                    setCurrentNode(id);
+                    setIsOpen(true);
+                  }}
+                >
+                  <FaPlus />
+                </div>
+                <div
+                  style={{ cursor: "pointer", marginLeft: "5px" }}
+                  onClick={() => {
+                    onDeleteNode(id);
+                  }}
+                >
+                  <FaTrash />
+                </div>
+              </div>
+            </div>
+          ),
+        },
+        position: {
+          x: 300 + Math.floor(Math.random() * 130),
+          y: 100 + Math.floor(Math.random() * 100),
+        },
         sourcePosition: Position.Right,
         targetPosition: Position.Left,
       },
@@ -79,7 +111,7 @@ const TouchDeviceFlow = () => {
     setEdges((eds) =>
       addEdge(
         {
-          source: currentNode.id,
+          source: currentNode,
           sourceHandle: null,
           target: id,
           targetHandle: null,
@@ -93,8 +125,8 @@ const TouchDeviceFlow = () => {
     setNameNode("");
   };
 
-  const onDeleteNode = (node) => {
-    console.log(node);
+  const onDeleteNode = (idNode) => {
+    setNodes((nds) => nodes.filter((node) => node.id !== idNode));
   };
 
   return (
@@ -107,13 +139,13 @@ const TouchDeviceFlow = () => {
         onEdgesChange={onEdgesChange}
         className="touchdevice-flow"
         fitView
-        onNodeClick={(e, node) => {
-          setCurrentNode(node);
-          setIsOpen(true);
-        }}
-        onNodesDelete={(node) => {
-          onDeleteNode(node);
-        }}
+        // onNodeClick={(e, node) => {
+        //   setCurrentNode(node);
+        //   setIsOpen(true);
+        // }}
+        // onNodesDelete={(node) => {
+        //   onDeleteNode(node);
+        // }}
       />
 
       <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)} ƒ>
